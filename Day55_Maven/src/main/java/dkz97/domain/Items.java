f9e8b3ef9da2470a42895a67ba2e4197ae2cd83f
dkz97 <48637588+dkz97@users.noreply.github.com>
2019-06-29 16:41:17 +0800

    Add files via upload

---
+++ Day55_Maven/src/main/java/dkz97/domain/Items.java
@@ -0,0 +1,31 @@
+package dkz97.domain;
+
+public class Items {
+
+    private Integer id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Items{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Day55_Maven/src/main/webapp/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hellp</title>
+</head>
+<body>
+
+hello.......
+</body>
+</html>
